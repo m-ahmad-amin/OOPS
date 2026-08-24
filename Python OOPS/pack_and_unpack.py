028c145ad766c3c09d05ce5cebf96861ed863295
m-ahmad-amin <mahmadamindw@gmail.com>
2026-08-24 21:21:51 +0500

    Update pack and unpack

--- Python OOPS/pack_and_unpack.py
+++ Python OOPS/pack_and_unpack.py
@@ -1,54 +1,54 @@
-# # Packing
-# x = 10, 20, 30
-# print(type(x))
-# print(x)
+# Packing
+x = 10, 20, 30
+print(type(x))
+print(x)
 
-# # Unpackings
-# # Tuple
-# nums = (1, 2, 3)
-# a, b, c = nums
-# print(a, b, c)
+# Unpackings
+# Tuple
+nums = (1, 2, 3)
+a, b, c = nums
+print(a, b, c)
 
-# # List
-# a, b, c = [1, 2, 3]
-# print(a, b, c)
+# List
+a, b, c = [1, 2, 3]
+print(a, b, c)
 
-# # Extended Unpacking
-# a, *b, c, d = [1, 2, 3, 4, 5, 6]
-# print(b, type(b))
+# Extended Unpacking
+a, *b, c, d = [1, 2, 3, 4, 5, 6]
+print(b, type(b))
 
-# # Packing in function parameters: *args
-# def sum(*nums):
-#     sum = 0
-#     for num in nums:
-#         sum += num
-#     return sum
+# Packing in function parameters: *args
+def sum(*nums):
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum
 
-# print(sum(1, 2))
+print(sum(1, 2))
 
-# # Unpacking (Spread Operator)
-# def sum(*nums):
-#     sum = 0
-#     for num in nums:
-#         sum += num
-#     return sum
+# Unpacking (Spread Operator)
+def sum(*nums):
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum
 
-# nums = [1, 2, 3, 4, 5]
-# print(sum(*nums))
+nums = [1, 2, 3, 4, 5]
+print(sum(*nums))
 
-# # **kwargs (Dictionary packing)
-# def person(**props):
-#     print(type(props))
-#     print(props['name'])
+# **kwargs (Dictionary packing)
+def person(**props):
+    print(type(props))
+    print(props['name'])
 
-# person(name="Ahmad", age=10)
+person(name="Ahmad", age=10)
 
-# # Dictionary unpacking
-# def person(name, age):
-#     print(name, age)
+# Dictionary unpacking
+def person(name, age):
+    print(name, age)
 
-# p = {'name': 'Ahmad', 'age': 21}
-# person(**p)
+p = {'name': 'Ahmad', 'age': 21}
+person(**p)
 
 list1 = [1, 2, 3]
 list2 = [4, 5, 6]
