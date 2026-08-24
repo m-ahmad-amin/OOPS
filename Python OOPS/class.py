73dba0046ac1d79eb9f1bfdc5199d58fcd1e2fff
m-ahmad-amin <mahmadamindw@gmail.com>
2026-08-24 21:42:24 +0500

    Add Person class

--- Python OOPS/class.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-p1 = Person('Ahmad', 19)
-p2 = Person('Ali', 20)
-print(p1.name, p1.age, p2.name, p2.age)
