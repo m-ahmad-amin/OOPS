b48acb596ec90ca8e7754789ddf0ca846b000608
m-ahmad-amin <mahmadamindw@gmail.com>
2026-08-25 13:40:02 +0500

    Update encapsulation

--- Python OOPS/encapsulation.py
+++ Python OOPS/encapsulation.py
@@ -9,17 +9,21 @@ class BankAccount:
     def withdraw(self, amount):
         self.__balance -= amount
 
+    def get_balance(self):
+        return self.__balance
+
 ahmad_account = BankAccount('Ali', 10000)
 
-print(ahmad_account._BankAccount__balance)
+# print(ahmad_account._BankAccount__balance) # don't do this
+print(ahmad_account.get_balance())
 
 ahmad_account.deposit(1000)
 
-print('after depositing 1000, balance: ', ahmad_account._BankAccount__balance)
+print('after depositing 1000, balance: ', ahmad_account.get_balance())
 
 ahmad_account.withdraw(2000)
 
-print('after withdrawing 2000, balance: ', ahmad_account._BankAccount__balance)
+print('after withdrawing 2000, balance: ', ahmad_account.get_balance())
 
 """
 name
